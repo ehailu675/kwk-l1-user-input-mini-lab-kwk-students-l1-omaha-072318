486eb3a50ef53f4dc915f90f12771746a59a65d2
ehailu675 <elisabethahailu@gmail.com>
2018-07-24 18:29:22 +0000

    automatically backed up by learn

--- practice.rb
+++ practice.rb
@@ -3,7 +3,7 @@ def greeting
   your_name = "fellow seres humanos"
   my_name = "Elisabeth"
   time_of_day = "life"
-  puts " #{local_greeting} #{your_name}, I'm #{my_name} how's life" 
+  puts " #{local_greeting} #{your_name}, I'm #{my_name} how's #{time_of_day}?" 
 end
 
 greeting
